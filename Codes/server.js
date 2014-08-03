fea6507a93ae7408ca231a2389e59acb94195313
rcadaoas <romeo.mail7@gmail.com>
2014-08-03 22:34:43 +0400

    Node.js Real Time Chat: Inserting Data (Part 4/7)

--- Codes/server.js
+++ Codes/server.js
@@ -1,3 +1,30 @@
 // console.log('Worked');
-var mongo = require('mongodb').MongoClient,
-	client = require('socket.io').listen(1141).sockets;
+ var mongo = require('mongodb').MongoClient,
+ 	 client = require('socket.io').listen(8080).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chat',function(err,db) {
+	if(err) throw err;
+
+	client.on('connection',function(socket){
+		
+		var col= db.collection('messages');
+
+		//Wait for input
+		socket.on('input', function(data){
+			var name = data.name,
+				message = data.message,
+				whitespacePattern=/^\s*$/;
+
+				if(whitespacePattern.test(name)||whitespacePattern.test(message)){
+					console.log('invalid');
+				}	
+				else{
+					col.insert({name:name, message:message},function(){
+						console.log('Inserted');
+					});
+
+				}
+		});
+	});
+ });
+
